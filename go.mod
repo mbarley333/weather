8cd70b964e55a1c78f0af8e78592f80698adaa1d
mbarley333 <jacksonkato808@gmail.com>
2021-05-19 21:36:50 -1000

    mock api response

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module weather
 
 go 1.16
+
+require github.com/jarcoal/httpmock v1.0.8 // indirect
--- weather.go
+++ weather.go
@@ -16,23 +16,25 @@ func Get(location string) ([]byte, error) {
 
 	//get api response
 	resp, err := http.Get(fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?q=%s&appid=3b814c61996538f2e8a2b921e23bbb0a", location))
+
 	if err != nil {
-		return nil, fmt.Errorf("Something went wrong.  Please try again later.  %v", err)
+		return nil, fmt.Errorf("something went wrong.  Please try again later.  %v", err)
 	}
 
 	//fmt.Println(resp.StatusCode)
 
 	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("Unexpected status code.  %v", resp.StatusCode)
+		return nil, fmt.Errorf("unexpected status code.  %v", resp.StatusCode)
 	}
 
 	//fmt.Printf("%+v", resp.Body)
 
 	data, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return nil, fmt.Errorf("Something went wrong.  Please try again later.  %v", err)
+		return nil, fmt.Errorf("something went wrong.  Please try again later.  %v", err)
 	}
-	fmt.Printf("%s", data)
+
+	//fmt.Printf("%s", data)
 
 	return data, nil
 }
